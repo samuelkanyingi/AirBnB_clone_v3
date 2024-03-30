20437f2f980c08c7981c26d169550657a5bbaf0b
julioceaseless <julioceaseless@gmail.com>
2024-03-30 23:37:09 +0000

    disabled the implementation of object counter because it is incorrect

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,6 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        storage_get_count
         except Exception as e:
             pass
 
@@ -65,9 +64,9 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
-
+    """"
     def count(self, cls=None):
-        """A method to count the number of objects in storage"""
+        # A method to count the number of objects in storage
         if cls is None:
             count = sum([self.__session.query(cls).count()
                         for cls in classes.values()])
@@ -76,6 +75,7 @@ class FileStorage:
         else:
             count = 0
         return count
+    """
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
