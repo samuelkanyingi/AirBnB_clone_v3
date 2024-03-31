85a27b9051d042c36598259fd6d8835a81bf5a5b
julioceaseless <julioceaseless@gmail.com>
2024-03-31 13:02:18 +0000

    small changes to doc strings

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,5 +6,5 @@ from flask import Blueprint
 # create blueprint
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
-# import the routes after creating the Blueprint
+# import the routes after creating the Blueprints
 from api.v1.views.index import *
