01de8fbe2fe01e2a085562d70a558265c954e4e6
julioceaseless <julioceaseless@gmail.com>
2024-03-31 02:16:35 +0000

    added route for stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 """ create routes for index page """
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
     """ creates route to 'api/v1/status'"""
     status_dict = {"status": "OK"}
-    return status_dict
+    return jsonify(status_dict)
+
+
+@app_views.route("/stats")
+def stats():
+    """retrieve the number of each object by type"""
+    stats_dict = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')
+            }
+    return jsonify(stats_dict)
