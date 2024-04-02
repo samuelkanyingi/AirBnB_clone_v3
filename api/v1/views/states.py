b454739475d7f3a9e70b859606aa38f2ef743fdc
julioceaseless <julioceaseless@gmail.com>
2024-04-02 03:03:53 +0000

    CRUD methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""create routes for index page"""
+"""create routes for state page"""
 from api.v1.views import app_views
 from flask import abort, jsonify, request, make_response
 from models import storage
