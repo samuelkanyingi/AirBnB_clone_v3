97fadce51e06e44dfc3a1aac7e0d2c4c31c0d4a9
julioceaseless <julioceaseless@gmail.com>
2024-04-02 04:27:28 +0000

    add leading space to doc_string

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""create routes for index page """
+""" create routes for index page """
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
@@ -7,14 +7,14 @@ from models import storage
 
 @app_views.route("/status/")
 def status():
-    """creates route to 'api/v1/status'"""
+    """ creates route to 'api/v1/status'"""
     status_obj = {"status": "OK"}
     return jsonify(status_obj)
 
 
 @app_views.route("/stats", methods=['GET'])
 def stats():
-    """retrieve the number of each object by type"""
+    """ retrieve the number of each object by type"""
     stats_obj = {
             "amenities": storage.count('Amenity'),
             "cities": storage.count('City'),
