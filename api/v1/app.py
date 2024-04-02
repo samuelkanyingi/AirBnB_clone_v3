318c6b10ac69b0ebaad0ad486e2361e560c8c486
julioceaseless <julioceaseless@gmail.com>
2024-04-02 02:59:00 +0000

    Revert "litte fix"
    This reverts commit 07b590835cfccf077025e3d3b24d990c6a6db57e.

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,7 +14,7 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown_appcontext(exception=None):
     """close database connection and release resources"""
     storage.close()
 
@@ -30,7 +30,7 @@ def not_found(e):
 
 if __name__ == "__main__":
     """ run only when called directly and set default configs"""
-    HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(os.getenv('HBNB_API_PORT', 5000))
+    HOST = os.genev('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(os.genenv('HBNB_API_PORT', 5000))
 
     app.run(debug=True, threaded=True, host=HOST, port=PORT)
